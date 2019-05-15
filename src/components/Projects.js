cc233047466e23cf48f17eacd0a467d8543285a0
sbrij001 <sbrij001@yahoo.com>
2019-05-15 19:18:58 -0400

    changed project image size

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,17 +8,17 @@ const Projects = props => {
 
         <div class="column">
           <h2 className="bounce">Bounce</h2>
-          <a href="https://youtu.be/Zzv5JLOOyXQ"><img src="bounceGif.gif" className="inline-photo show-on-scroll" alt="bounceGif"/></a>
+          <a href="https://youtu.be/Zzv5JLOOyXQ"><img src="bounceGif.gif" className="inline-photo show-on-scroll" alt="bounceGif" height="166"/></a>
         </div>
 
         <div class="column">
           <h2 className="boomFont">Resume</h2>
-          <a href="https://docs.google.com/document/d/e/2PACX-1vQ5oBLLuwZWrI7EHqhhOP1vQ0xRdSMxsXE3AgySRAg8ScLy1miWjkoenC5zkg2rUqzCC6z2qEBPiMtV/pub"><img src="resume.png" className="inline-photo show-on-scroll" alt="bounceGif"/></a>
+          <a href="https://docs.google.com/document/d/e/2PACX-1vQ5oBLLuwZWrI7EHqhhOP1vQ0xRdSMxsXE3AgySRAg8ScLy1miWjkoenC5zkg2rUqzCC6z2qEBPiMtV/pub"><img src="resume.png" className="inline-photo show-on-scroll" alt="resume" height="166"/></a>
         </div>
 
         <div class="column">
           <h2 className="boomFont">Boom</h2>
-          <a href="https://bo0m.herokuapp.com/"><img src="boomGifRevised.gif" className="inline-photo show-on-scroll" alt="bounceGif"/></a>
+          <a href="https://bo0m.herokuapp.com/"><img src="boomGifRevised.gif" className="inline-photo show-on-scroll" alt="bounceGif" height="166"/></a>
         </div>
 
 
