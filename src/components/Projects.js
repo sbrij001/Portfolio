847ec58d24dd7a8f547cb6818c6db8abf46ade8d
sbrij001 <sbrij001@yahoo.com>
2019-09-17 23:22:29 -0400

    imported semantic ui cards to projects component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,27 +1,45 @@
 import React from "react";
-import { Link, withRouter } from "react-router-dom";
+import { Card, Image } from 'semantic-ui-react';
+
+import { withRouter } from "react-router-dom";
+
 import projects from '../style/projects.css';
 
 const Projects = props => {
   return (
-      <div class="row">
+    <div class="row">
+      <Card>
+        <Card.Content extra> <h1>Bounce</h1> </Card.Content>
+        <a href="https://youtu.be/Zzv5JLOOyXQ">
+        <Image
+          className="inline-photo show-on-scroll"
+          alt="bounceGif"
+          src="bounceGif.gif"
+        />
+        </a>
+      </Card>
 
-        <div class="column">
-          <h2 className="bounce">Bounce</h2>
-          <a href="https://youtu.be/Zzv5JLOOyXQ"><img src="bounceGif.gif" className="inline-photo show-on-scroll" alt="bounceGif" height="166"/></a>
-        </div>
+      <Card> <Card.Content extra> <h1>Boom</h1>
+        </Card.Content>
+        <a href="https://bo0m.herokuapp.com">
+        <Image
+          className="inline-photo show-on-scroll"
+          alt="boomGif"
+          src="boomGifRevised.gif"
+         />
+        </a>
+      </Card>
 
-        <div class="column">
-          <h2 className="boomFont">Resume</h2>
-          <a href="https://docs.google.com/document/d/e/2PACX-1vQ5oBLLuwZWrI7EHqhhOP1vQ0xRdSMxsXE3AgySRAg8ScLy1miWjkoenC5zkg2rUqzCC6z2qEBPiMtV/pub"><img src="resume.png" className="inline-photo show-on-scroll" alt="resume" height="166"/></a>
-        </div>
-
-        <div class="column">
-          <h2 className="boomFont">Boom</h2>
-          <a href="https://bo0m.herokuapp.com/"><img src="boomGifRevised.gif" className="inline-photo show-on-scroll" alt="bounceGif" height="166"/></a>
-        </div>
-
-
+      <Card>
+        <Card.Content extra><h1>Resume</h1></Card.Content>
+        <a href="https://bo0m.herokuapp.com">
+        <Image
+          className="inline-photo show-on-scroll"
+          alt="boomGif"
+          src="resumeJPG.jpg"
+         />
+        </a>
+      </Card>
       </div>
   );
 };
