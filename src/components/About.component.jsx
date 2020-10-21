865280bf7ce4f2e06043a98216ffd026b8d8d83b
sbrij001 <shivani.brijmohan@gmail.com>
2020-10-21 16:37:45 -0400

    home page and about page mobile / desktop edits

--- src/components/About.component.jsx
+++ src/components/About.component.jsx
@@ -5,23 +5,23 @@ class About extends React.Component{
   render(){
     return(
       <div className='container'>
-        <h1 id='title'>SHIVANI BRIJMOHAN</h1>
+        <h1 className='govt-name'>SHIVANI BRIJMOHAN</h1>
         <div className='intro'>
           <p>
             In dinner party conversation, I dabble in many things from frontend engineering, to hospitality, to cofounding my own company. In LinkedIn language, I'm a freelance frontend engineer with a passion for action. Or you can call me Shivani.
-          </p>
+          </p><br></br>
           <p>
             I like to tell stories that complicate things but also excite and connect people.
-          </p>
+          </p><br></br>
           <p>
             I’ve coded for nonprofit organizations, Bose Sound Company, The Company, JP Morgan, The StartupBus, among others.
-          </p>
+          </p><br></br>
           <p>
-            I know my way around HTML, CSS, Ruby on Rails, Javascript, and frameworks like React and Redux
-          </p>
+            I know my way around HTML, CSS, Ruby on Rails, Javascript, and frameworks like React and Redux.
+          </p><br></br>
           <p>
             I'm based out of New York City and am available for frontend engineering, fullstack engineering, and great conversation.
-          </p>
+          </p><br></br>
           <p>
             Email me at shivani.brijmohan@gmail.com
           </p>
--- src/components/Navbar.component.jsx
+++ src/components/Navbar.component.jsx
@@ -4,23 +4,26 @@ import '../style/styles.navbar.css';
 
 const Navbar = props => {
   return (
-    <div id="navbar">
-      <Link to="/banner">
-        <span className="gray">Shivani Brijmohan </span>
-      </Link>
-    <ul class="list">
-      <Link class="projects" to="/projects">
-         Projects
-      </Link>
-      <Link class="about" to="/about">
-         About
-      </Link>
-      <Link to="/contact">
-         Contact
-      </Link>
-    </ul>
-    </div>
+    <div id="navbar" class="top-nav">
+      <ul class="list">
+        <Link to="/banner">
+          <span className="gray">Shivani Brijmohan </span>
+        </Link>
+        <div className="top-nav-right">
+          <Link class="projects" to="/projects">
+            Projects
+          </Link>
+          <Link class="about" to="/about">
+            About
+          </Link>
+          <Link to="/contact">
+            Contact
+          </Link>
+        </div> {/* end .top-nav-right  */}
+      </ul> {/* end .list  */}
+    </div> /* end #navbar  */
   );
 };
+
 
 export default withRouter(Navbar);
